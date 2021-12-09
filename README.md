d50c486122b936d05fcc73d6c243188170abbdcf
jams10 <gjwpals2817@daum.net>
2021-12-09 17:04:46 +0900

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# hw3d
+
+#### Reference Video
+
+##### [C++ 3D DirectX Tutorial by Chili](https://www.youtube.com/playlist?list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD)
---
+++ hw3d/WinMain.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************************
+*	Chili Direct3D Engine																  *
+*	Copyright 2018 PlanetChili <http://www.planetchili.net>								  *
+*																						  *
+*	This file is part of Chili Direct3D Engine.											  *
+*																						  *
+*	Chili Direct3D Engine is free software: you can redistribute it and/or modify		  *
+*	it under the terms of the GNU General Public License as published by				  *
+*	the Free Software Foundation, either version 3 of the License, or					  *
+*	(at your option) any later version.													  *
+*																						  *
+*	The Chili Direct3D Engine is distributed in the hope that it will be useful,		  *
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of						  *
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  *
+*	GNU General Public License for more details.										  *
+*																						  *
+*	You should have received a copy of the GNU General Public License					  *
+*	along with The Chili Direct3D Engine.  If not, see <http://www.gnu.org/licenses/>.    *
+******************************************************************************************/
+#include "Window.h"
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow )
+{
+	Window wnd( 800, 300,  L"Donkey Fart Box" );
+	Window wnd2( 200, 650, L"Donkey Fart Box2" );
+
+	MSG msg;
+	BOOL gResult;
+	while( (gResult = GetMessage( &msg, nullptr, 0, 0 )) > 0 )
+	{
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
+	if( gResult == -1 )
+	{
+		return -1;
+	}
+
+	return msg.wParam;
+}
